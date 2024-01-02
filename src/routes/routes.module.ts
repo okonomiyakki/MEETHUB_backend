643f9dc34f4f82e9491cc627fe262e03b16c7315
okonomiyakki <parkji12532@naver.com>
2024-01-02 23:08:40 +0900

    feat: nsetJS Axios Http 모듈 추가

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoutesController } from './routes.controller';
 import { RoutesService } from './routes.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [RoutesController],
   providers: [RoutesService],
 })
