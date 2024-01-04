057fba8e87741421dbc6b6ff3208f9ff70d45ed7
okonomiyakki <parkji12532@naver.com>
2024-01-05 02:23:43 +0900

    feat: 로컬, 배포버전 cross env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RoutesModule } from './routes/routes.module';
+import { ConfigModule } from '@nestjs/config';
+
+// console.log(`.env.${process.env.NODE_ENV}`);
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    RoutesModule,
+  ],
 })
 export class AppModule {}
